3fafb12be463c0c6ee1a7d76903ec4d77d724922
Preethamsm <154796834+Preethamsm@users.noreply.github.com>
2026-03-05 12:07:10 +0530

    Add palindrome checking functionality

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+public class PalindromeString {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "racecar";
+        String str2 = "hello";
+
+        System.out.println(str1 + " is a palindrome: " + isPalindrome(str1));
+        System.out.println(str2 + " is a palindrome: " + isPalindrome(str2));
+    }
+}
