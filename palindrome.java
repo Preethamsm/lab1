2bf24b11300108368c2b06b87872cee348128025
Preethamsm <154796834+Preethamsm@users.noreply.github.com>
2026-03-05 12:29:48 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -16,7 +16,7 @@ public class palindrome {
     public static void main(String[] args) {
         String str1 = "racecar";
         String str2 = "hello";
-
+        System.out.println(str1 + " is a palindrome: " + isPalindrome(str1));
         System.out.println(str1 + " is a palindrome: " + isPalindrome(str1));
         System.out.println(str2 + " is a palindrome: " + isPalindrome(str2));
     }
